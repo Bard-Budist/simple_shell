b97c2b2e9746a0791b4e20f7605f35f1a2bab2e6
GiovanniAyala <op7puro@live.com>
2019-11-18 20:01:20 -0500

    Add head file

---
+++ fun.h
@@ -0,0 +1,19 @@
+#ifndef header_file
+#define header_file
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <limits.h>
+#include <sys/stat.h>
+#include <libgen.h>
+void TokensGenator(char *dates);
+void free_grid(char **grid);
+size_t _getline(char *string);
+void functionLs();
+int executeCommand(char *file);
+int checkCommand (char *path, char *ps);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+#endif
