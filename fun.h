03a6d74bd65a62a2f167a47c0e829b6a0cadac9d
andres0191 <andres_felipe0191@hotmail.com>
2019-11-27 21:38:36 +0000

    modify line 15 whit function of type void, and line 17 with space between function and date of input

--- fun.h
+++ fun.h
@@ -12,9 +12,9 @@
 #include <signal.h>
 void TokensGenator(char *dates);
 size_t _getline(char *string);
-void functionLs();
+void functionLs(void);
 int executeCommand(char *file);
-void checkCommand (char *path, char **Tokens);
+void checkCommand(char *path, char **Tokens);
 char *_strcat(char *dest, char *src);
 char *_strcpy(char *dest, char *src);
 void free_grid(char **grid, int height);
